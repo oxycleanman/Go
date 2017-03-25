46e8e86c3b922addb568d4a0626021bd4c25a766
Chris Leatherwood <Chris Leatherwood>
2017-03-25 00:29:57 -0400

    Set up repo on personal pc

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,8 +2,11 @@ package main
 
 import "net/http"
 
+const myVar string = "My Test String"
+const myVar2 string = "My Test String 2"
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://ma-returns.apps-np.homedepot.com/?id=" + r.URL.Path[1:], 301)
+	http.Redirect(w, r, "https://ma-returns.apps-np.homedepot.com/?id="+r.URL.Path[1:], 301)
 }
 
 func main() {
