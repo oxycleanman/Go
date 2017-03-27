06d20f0bc4ed1e3fb90f18265f6732e381b5f678
Leatherwood, Christopher <christopher_leatherwood@homedepot.com>
2017-03-27 17:10:22 -0400

    Monday's work

--- src/MA-Returns/main.go
+++ src/MA-Returns/main.go
@@ -1,17 +1,52 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
-	"os"
-	"strings"
+	"net/http"
 )
 
-func main() {
-	fmt.Println("Enter a value: ")
-	reader := bufio.NewReader(os.Stdin)
+type replyStructure struct {
+	Data   string
+	Status int
+	Id     string
+}
 
-	text, _ := reader.ReadString('\n')
-	text = strings.Replace(text, "\r\n", "", -1)
-	fmt.Println("You entered: " + text)
+func homePage(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "GET" {
+		idValue := req.URL.Query().Get("id")
+		myReply := replyStructure{Data: "This is a test of the data attribute", Status: 200, Id: idValue}
+		bs, _ := json.Marshal(myReply)
+		w.Write(bs)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		/*w.Write([]byte(`
+			<html>
+				<div>
+					Error: Unsupported Method: ` + req.Method + `
+				</div>
+			</html>
+		`))*/
+	}
+
+}
+
+func orderPage(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte(`
+		<html>
+			<title>This is the test page</title>
+			<div>
+				<p>
+					This is the orders page
+				</p>
+			</div>
+		</html>
+		`))
+}
+
+func main() {
+	fmt.Println("Server started on port 8080")
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/orders", orderPage)
+	http.ListenAndServe(":8080", nil)
 }
